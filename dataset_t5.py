7ab9c158ba77cbdd22b373f5127248f4e95e0b88
galimzyanov <gal_timur@yahoo.com>
2024-07-05 12:41:43 +0000

    Work with T5

--- dataset_t5.py
+++ dataset_t5.py
@@ -18,14 +18,38 @@ class CSVDataset(Dataset):
         code = code.replace("METHOD_NAME", "<extra_id_0>")
         label = self.data.iloc[idx]['label']
         label = label.replace(" ", "_")
-        label = f"<extra_id_0> {label}"
-        label_ids = self.tokenizer(label, return_tensors="pt").input_ids.to(self.device)
+        label_ids = self.tokenizer(f"<extra_id_0> {label}").input_ids
 
-        input_ids = self.tokenizer(code, return_tensors="pt").input_ids.to(self.device)
+        input_ids = self.tokenizer(code).input_ids
         # inputs = self.tokenizer(code, max_length=self.max_length, padding='max_length', truncation=True, return_tensors='pt')
         # input_ids = inputs['input_ids'].squeeze(0)
 
         return {
             'input_ids': input_ids,
-            'label_ids': label_ids
+            'label_ids': label_ids,
+            'label': label
         }
+
+
+def collate_fn(batch, tokenizer, device):
+    input_ids = [item["input_ids"] for item in batch]
+    label_ids = [item["label_ids"] for item in batch]
+    labels = [item["label"] for item in batch]
+    max_length_input = max(len(ids) for ids in input_ids)
+    max_length_label = max(len(ids) for ids in label_ids)
+
+    input_ids_padded = []
+    label_ids_padded = []
+
+    for (input_ids, label_ids) in zip(input_ids, label_ids):
+        padding_input_length = max_length_input - len(input_ids)
+        padding_label_length = max_length_label - len(label_ids)
+
+        input_ids_padded.append(input_ids + [tokenizer.pad_token_id] * padding_input_length)
+        label_ids_padded.append(label_ids + [tokenizer.pad_token_id] * padding_label_length)
+
+    return {
+        "input_ids": torch.tensor(input_ids_padded).to(device),
+        "attention_mask": torch.tensor(label_ids_padded).to(device),
+        "labels": labels
+    }
--- encoder_based.py
+++ encoder_based.py
@@ -1,5 +1,12 @@
 from transformers import T5ForConditionalGeneration, AutoTokenizer
 import re
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from functools import partial
+
+from dataset_t5 import CSVDataset, collate_fn
+from eval_metrics import calc_metrics
+
 #%%
 checkpoint = "Salesforce/codet5p-220m"
 device = "cuda" # for GPU usage or "cpu" for CPU usage
@@ -7,36 +14,46 @@ device = "cuda" # for GPU usage or "cpu" for CPU usage
 
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 model = T5ForConditionalGeneration.from_pretrained(checkpoint).to(device)
+collate_fn_tok = partial(collate_fn, tokenizer=tokenizer, device=device)
 
 #%%
 
-function_body = '''
-def <extra_id_0>():
-    print("hello_world")
-'''
+val_file = "/data/data/train_val.csv"
+val_dataset = CSVDataset(val_file, tokenizer)
+val_dl = DataLoader(val_dataset, batch_size=50, shuffle=True, collate_fn=collate_fn_tok)
+item = next(iter(val_dl))
 
 #%%
-# inputs = tokenizer.encode("def print_hello_world():<extra_id_0>", return_tensors="pt").to(device)
-inputs = tokenizer.encode(function_body, return_tensors="pt").to(device)
-outputs = model.generate(inputs, max_length=10)
-prediction = tokenizer.decode(outputs[0], skip_special_tokens=True)
-print(prediction)
-# ==> print "Hello World"
-# match = re.search(r'def (\w+)', prediction)
-# function_name = match.group(1)
-# print(function_name)
+
+predictions = []
+labels = []
+idx = 0
+
+for item in tqdm(val_dl):
+    outputs = model.generate(item["input_ids"], max_length=10)
+    prediction = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    prediction = [re.sub(r"^[^a-zA-Z]*|[^a-zA-Z]*$", "", pred) for pred in prediction]
+    prediction = [(pred.split(" ")[0]).lower().split("_") for pred in prediction]
+    predictions.extend(prediction)
+
+    labels_splitted = [label.split("_") for label in item["labels"]]
+    labels.extend(labels_splitted)
+    if idx > 10:
+        break
+    idx += 1
+
 #%%
 
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+calc_metrics(predictions, labels)
 
-tokenizer = T5Tokenizer.from_pretrained("google-t5/t5-small")
-model = T5ForConditionalGeneration.from_pretrained("google-t5/t5-small")
+#%%
 
-input_ids = tokenizer("The <extra_id_0> walks in <extra_id_1> park", return_tensors="pt").input_ids
-labels = tokenizer("<extra_id_0> cute dog <extra_id_1> the <extra_id_2>", return_tensors="pt").input_ids
-
-# the forward function automatically creates the correct decoder_input_ids
-loss = model(input_ids=input_ids, labels=labels).loss
-loss.item()
+#
+# input_ids = tokenizer("The <extra_id_0> walks in <extra_id_1> park", return_tensors="pt").input_ids
+# labels = tokenizer("<extra_id_0> cute dog <extra_id_1> the <extra_id_2>", return_tensors="pt").input_ids
+#
+# # the forward function automatically creates the correct decoder_input_ids
+# loss = model(input_ids=input_ids, labels=labels).loss
+# loss.item()
 
 #%%
